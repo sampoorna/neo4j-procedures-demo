7824c544aed23c8dfad2884420c741f2a6b4ebef
sampoorna <sampoorna.biswas01@gmail.com>
2017-11-24 13:15:54 -0800

    DEBUG: Bug-free stats.degree()

--- src/main/java/stats/GetDegree.java
+++ src/main/java/stats/GetDegree.java
@@ -1,17 +1,22 @@
 package stats;
 
+import java.util.*;
+import java.util.stream.*;
+import org.neo4j.graphdb.*;
+import org.neo4j.procedure.*;
+import org.neo4j.logging.Log;
+
 public class GetDegree {
 
-    @Context private GraphDatabaseService db;
+    @Context public org.neo4j.graphdb.GraphDatabaseService db;
 
-    // Result class
     public static class Degree {
-        public String label;
-        // note, that "int" values are not supported
+        public final String label;
         public long count, max, min = Long.MAX_VALUE;
-        
-        // method to consume a degree and compute min, max, count
-        private void add(long degree) {
+
+        Degree(String label) { this.label = label; }
+
+        void add(int degree) {
             if (degree < min) min = degree;
             if (degree > max) max = degree;
             count ++;
@@ -19,18 +24,14 @@ public class GetDegree {
     }
 
     @Procedure
-
-    public Stream<Degree> degree(String label) {
-        // create holder class for results
+    // not needed here @PerformsWrites
+    public Stream<Degree> degree(@Name("label") String label) {
         Degree degree = new Degree(label);
-        // iterate over all nodes with label
-        try (ResourceIterator it = db.findNodes(Label.label(label))) {
+        try (ResourceIterator<Node> it = db.findNodes(Label.label(label))) {
             while (it.hasNext()) {
-                // submit degree to holder for consumption (i.e. max, min, count)
-                d.add(it.next().getDegree());
+                degree.add(it.next().getDegree());
             }
         }
-        // we only return a "Stream" of a single element in this case.
         return Stream.of(degree);
     }
 }
